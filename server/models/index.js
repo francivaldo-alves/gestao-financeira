02925fc18948e20c9bf2f445a42b579db6e2735f
Francivaldo <francivaldo.alves@gmail.com>
2025-12-01 22:16:51 -0300

    ajustes no index

--- server/models/index.js
+++ server/models/index.js
@@ -1,28 +1,44 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const config = require('../config/database.js');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { sequelize } = require("./models");
+const authRoutes = require("./routes/auth");
+const transactionRoutes = require("./routes/transactions");
+const path = require("path");
 
-const db = {};
-const sequelize = config.sequelize;
+dotenv.config();
 
-fs
-    .readdirSync(__dirname)
-    .filter(file => {
-        return (file.indexOf('.') !== 0) && (file !== 'index.js') && (file.slice(-3) === '.js');
-    })
-    .forEach(file => {
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-        db[model.name] = model;
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/transactions", transactionRoutes);
+
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "public")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API do Sistema de Gestão Financeira");
+  });
+}
+
+// Sync database and start server
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Banco de dados conectado (sync alter).");
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
     });
-
-Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+  })
+  .catch((err) => {
+    console.error("Erro na conexão com o banco de dados:", err);
+  });
